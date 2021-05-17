90977a0fb21b60a440ec734e53a04b16b24a9aca
jfchen <jfchen0724@163.com>
2021-05-17 23:11:49 +0800

    is work

--- springmvc-with-web-tutorial/src/main/java/com/jacky/mvc/service/TestService.java
+++ springmvc-with-web-tutorial/src/main/java/com/jacky/mvc/service/TestService.java
@@ -5,6 +5,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class TestService {
 
+    public TestService(){
+        System.err.println("root context service init ...");
+    }
+
     public String test(){
         return "test string";
     }
